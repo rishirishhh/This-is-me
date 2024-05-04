58b21bfb2e82250ea528284505bdae2c66dbaf30
rishi-19-04 <rishikavya19@gmail.com>
2024-05-05 01:27:36 +0530

    done with the code

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,20 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Golang, Javascript/Typescript, Python etc these days.
+    I have made some projects in Golang. I love Golang and Python. Anyway regardless of the language, I just like coding and learning about back-end engineering.
+    Here is my <a href="https://github.com/rishirishhh" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. It was a 4 year course. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to play poker here. I bring all my friends with me and the game often becomes quite intensive`,
+  tv: `That's my TV. I've been watching Netflix and Youtube a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1ua4lQq8PdcQvg6XdhA4Ftvo3eWXRLUCT/view" target="_blank">Check it out?</a>
+  Contact me at rishikavya19@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of novels on my shelves. I like to read all kind of genres. I also read tech books,
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit my portfolio, just close the tab.`,
+};
