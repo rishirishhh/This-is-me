d81104133fb98018dae1e1255c52204c0832ff93
rishi-19-04 <rishikavya19@gmail.com>
2024-05-04 21:53:11 +0530

    added logic to display maps

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx"
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
@@ -16,3 +17,23 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
 k.loadSprite("map", "./map.png");
 
 k.setBackground(k.Color.fromHex("#311047"))
+
+k.scene("main", async () => {
+    const mapData = await (await fetch("./map.json")).json()
+    const layers = mapData.layers;
+    
+    const map = k.make([
+        k.sprite("map"),
+        k.pos(0),
+        k.scale(scaleFactor)
+    ]);
+
+    const player = k.make([
+        k.sprite("spritesheet", {anim: "idle-down"}),
+        k.area({
+            collisionIgnore
+        })
+    ])
+})
+
+k.go("main");
